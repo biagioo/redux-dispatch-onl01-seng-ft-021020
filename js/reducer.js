c60d4225c07b4d4c76823ab6cfccd01607a7aa69
biagioo <mendolia2328@gmail.com>
2020-06-22 14:07:08 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,33 @@
+let state = {count: 0};
+let action = {type: 'INCREASE_COUNT'}
+
 function changeState(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
-    default:
-      return state;
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
+  }
+ 
+function dispatch(action){
+  state = changeState(state, action)
+  return state
+}
+ 
+function render(){
+  document.body.innerText = state.count
+}
+
+const times = x => f => {
+  if (x > 0) {
+    f()
+    times (x - 1) (f)
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+times(100)(()=> dispatch(action))
+ 
+render()
 
-changeState(state, action)
+
